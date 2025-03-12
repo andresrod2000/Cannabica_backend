7b3395608e52f10caae4c6de588f3be00f320783
jhoanmartinez <jhoao1320@gmail.com>
2025-03-11 20:30:10 -0500

    quite lo dummy de usuario tura, controller y en app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Application } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import userRouter from "./routers/userRoute";
 import chatbotRoute from "./routers/chatbotRoute";
 import authRoutes from "./routers/authRoutes"; // Corregido: importación de rutas de autenticación
 import userRoutes from "./routers/userRoutes"; // Corregido: importación de rutas de usuario
@@ -12,9 +11,6 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 
-// Definir rutas
-app.use("/api", userRouter);
-console.log("✅ Ruta de usuario cargada en /api");
 
 app.use("/api/chatbot", chatbotRoute);
 console.log("✅ Ruta del chatbot cargada en /api/chatbot");
