f09f170717788f89c395dd4b7f095aab43bd7b3e
delaican <kdlaikanj@gmail.com>
2025-04-24 15:04:20 -0500

    remove test file

--- generate_token.ts
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const userId = "6808701bd8e35bb9960f95d2";
-const secret = "UNACONTRAPRUEBA123";
-
-const token = jwt.sign({ id: userId }, secret, { expiresIn: "1d" });
-console.log(token);
